dd452dbe9fab6e40191b37d9d7cc0961fe0e458d
Simon F Nilsson <simon.f.nilsson@me.com>
2026-04-25 18:07:54 +0200

    raylib example

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,42 @@
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
+#include <raylib.h>
 
 int main()
 {
-    std::cout << "Hello!\n";
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose()) // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        // TODO: Update your variables here
+        //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
 }
