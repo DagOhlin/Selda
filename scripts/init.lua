71e07e3dd313e3bf4fe85f29bd254328a4d79b6d
DagOhlin <ohlin.dag@gmail.com>
2026-04-28 22:09:02 +0200

    added blink in lua

--- scripts/init.lua
+++ scripts/init.lua
@@ -16,4 +16,11 @@ AddComponent(button, "Colour", 0, 0, 255, 255);
 AddComponent(button, "Text", "hello", 0, 0, 60, 255, 255, 78, 255);
 
 
+blink = CreateEntity();
+
+AddComponent(blink, "Position", 100.0, 100.0);
+AddComponent(blink, "Sprite", "textures/Blink2.png"); 
+AddComponent(blink, "Scale", 8.0);
+AddComponent(blink, "CharacterController", 30.0);
+
 AddComponent(button, "Clickable", "SuperDuperFunc", SuperDuperFunc);
